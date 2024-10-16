1aa3472fafa8029702b327727a87bb1c1082a91d
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-16 12:32:08 +0800

    Removed font files and imports, updated layout and page components

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
+'use client'
+import { useState } from "react";
+import { Card } from "@/components/Card";
 
 export default function Home() {
+  const [heading, setHeading] = useState('Welcome nowhere!');
+
+  const handleCardHover = (newHeading) => {
+    setHeading(newHeading); // Update the heading based on hover state
+  };
+
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1>
-        Welcome to nowhere
-      </h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1>{heading}</h1>
+      <Card
+        title="GitHub"
+        user="@fwedwicc"
+        onHover={handleCardHover}
+      />
     </div>
   );
 }
