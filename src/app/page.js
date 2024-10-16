c8d96104bfb9f46c427886001485e1f2a15dc703
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-16 12:48:58 +0800

    "Added import for Links constant and replaced single Card component with two lists of Card components mapped from Links.profession and Links.sites"

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
 import { Card } from "@/components/Card";
+import { Links } from "@/constants/data";
 
 export default function Home() {
   const [heading, setHeading] = useState('Welcome nowhere!');
@@ -12,11 +13,24 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <h1>{heading}</h1>
-      <Card
-        title="GitHub"
-        user="@fwedwicc"
-        onHover={handleCardHover}
-      />
+      <h4>Professions</h4>
+      {Links.profession.map((card, index) => (
+        <Card
+          key={index}
+          title={card.title}
+          user={card.user}
+          onHover={handleCardHover}
+        />
+      ))}
+      <h4>Live Sites</h4>
+      {Links.sites.map((card, index) => (
+        <Card
+          key={index}
+          title={card.title}
+          user={card.user}
+          onHover={handleCardHover}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/constants/data.js
@@ -0,0 +1,8 @@
+export const Links = {
+  profession: [
+    { title: "GitHub", user: "fwedwicc" }
+  ],
+  sites: [
+    { title: "Tech-Commr", user: "Frederick" }
+  ]
+}
