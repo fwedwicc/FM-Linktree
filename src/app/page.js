ad4410a944090a9af58ea54aedc418302b64c4cd
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-20 14:39:12 +0800

    "Update global CSS styles and hero section layout in page.js"

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,13 @@ import { Card } from "@/components/Card";
 import { Links } from "@/constants/data";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
 
   return (
     <div className="min-h-screen grid lg:grid-cols-2 grid-cols-1 gap-2">
       {/* Hero Section */}
       <div className="p-3 lg:h-screen h-[80vh]">
-        <div className='flex flex-col h-full justify-between bg-stone-900/20 border border-stone-900/40 rounded-3xl p-6'>
+        <div className='flex flex-col h-full justify-between bg-neutral-900 border border-stone-950 rounded-3xl p-6'>
           {/* Header */}
           <a href=''>
             <Image
@@ -22,19 +23,7 @@ export default function Home() {
           {/* Middle  Content */}
           <div className="space-y-6">
             <h1>FM Linktree</h1>
-            <div className="border-y border-stone-800 py-4 space-y-4">
-              <div className='flex items-start justify-between'>
-                {[
-                  { name: 'moreno.frederick.capiral@gmail.com', link: 'mailto:moreno.frederick.capiral@gmail.com' },
-                  { name: 'FM Portfolio', link: 'mailto:moreno.frederick.capiral@gmail.com' },
-                ].map((item, index) => (
-                  <a href={item.link} className='flex items-center text-indigo-400 text-xs underline' key={index}>{item.name}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="shrink-0 mt-[1.7px] size-3.5">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                  </a>
-                ))}
-              </div>
+            <div className="border-y border-dashed border-stone-800 py-4 space-y-6">
               <div className='flex items-start gap-3'>
                 <Image
                   className="mt-2 size-8 border-2 ring-2 ring-indigo-500 border-stone-950 rounded-full"
@@ -46,10 +35,22 @@ export default function Home() {
                   <p className='leading-none text-xs'>Front-end Developer • UI/UX Designer • IT Student</p>
                 </div>
               </div>
+              <div className='flex items-start justify-between'>
+                {[
+                  { name: 'moreno.frederick.capiral@gmail.com', link: 'mailto:moreno.frederick.capiral@gmail.com' },
+                  { name: 'FM Portfolio', link: 'https://fwedwicc.github.io/FM-Portfolio/' },
+                ].map((item, index) => (
+                  <a href={item.link} target='_blank' rel='noopener noreferrer' className='flex items-center text-indigo-400 text-xs underline' key={index}>{item.name}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="shrink-0 mt-[1.7px] size-3.5">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
           {/* Footer */}
-          <p className='text-xs'>2024 • Frederick Moreno</p>
+          <p className='text-xs'>{currentYear} • Frederick Moreno</p>
         </div>
       </div>
       {/* Links Section */}
