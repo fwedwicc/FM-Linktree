7db4facb1e0e9ed0f47636705d3c9716a7f26594
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-03-12 13:10:12 +0800

    Fix HTML entity for apostrophe in success message

--- src/app/page.js
+++ src/app/page.js
@@ -231,7 +231,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-14 text-green-500 mb-3">
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
             </svg>
-            <p className='text-md text-neutral-300'>Message sent successfully! <br />I'll get back to you soon!</p>
+            <p className='text-md text-neutral-300'>Message sent successfully! <br />I&apos;ll get back to you soon!</p>
           </div>
           <button onClick={closeSuccessModal} className='bg-indigo-500 hover:bg-indigo-600 md:py-2 py-1.5 md:px-3 px-2.5 rounded-md text-neutral-200 text-sm transition-all duration-300 ease-in-out'>Got it</button>
         </div>
